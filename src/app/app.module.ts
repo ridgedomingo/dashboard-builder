a3a875c7a75ba9a6a87ad9ebf93c3ac96fc22bf0
Ridge Christian Domingo <ridge.domingo@98labs.com>
2020-02-10 12:39:59 +0800

    Added dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GridsterModule } from 'angular-gridster2';
+
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridsterModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
