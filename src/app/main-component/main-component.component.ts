b7b214ae8768856494573aed4e4584061997f690
Ridge Christian Domingo <ridge.domingo@98labs.com>
2020-02-10 15:19:21 +0800

    Added toolbox

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IChartChoices {
+  data?: Array<any>;
+  type: string;
+}
+
 @Component({
   selector: 'app-main-component',
   templateUrl: './main-component.component.html',
   styleUrls: ['./main-component.component.scss']
 })
 export class MainComponentComponent implements OnInit {
+  public chartChoices = [
+    'Bar Chart',
+    'Pie Chart',
+  ];
+
+  public chartsList: Array<any> = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public addChart(selectedChart: string): void {
+    let chartData: IChartChoices = {} as IChartChoices;
+    chartData = {
+      type: selectedChart
+    };
+    this.chartsList.push(chartData);
   }
 
 }
